73a8f18d9552eb6c8e7a571a15a95db2276f3b6a
SungYG0612 <songyoungwan1903@gmail.com>
2025-06-10 14:04:02 +0900

    4fnd_uiFND_Data.2

--- Core/Src/project.c
+++ Core/Src/project.c
@@ -3,8 +3,8 @@
 
 //////////////////////////////////
 /*variable*/
-extern int flag;
-int FND_sel = 0;
+const int32_t FND_Number[10] = {0x82007d00,0xdb002400,0x1600e900,0x1a00e500,
+		0x4b00b400,0x2a00d500,0x2200dd00,0x9b006400,0x0200fd00,0x0a00f500};
 unsigned int uiFND_Data = 0;
 unsigned int FND_Buf[4]={};
 //////////////////////////////////
@@ -18,19 +18,13 @@ void project_initialization(void)
 
 void project_main(void)
 {
-	uiFND_Data = 3486;
-	FND_Buf[0] = uiFND_Data / 1000;
-	FND_Buf[1] = (uiFND_Data % 1000) / 100;
-	FND_Buf[2] = (uiFND_Data % 100) / 10;
-	FND_Buf[3] = uiFND_Data % 10;
+	uiFND_Data = 512;
+	FND_Buf[0] = FND_Number[uiFND_Data / 1000];
+	FND_Buf[1] = FND_Number[(uiFND_Data % 1000) / 100];
+	FND_Buf[2] = FND_Number[(uiFND_Data % 100) / 10];
+	FND_Buf[3] = FND_Number[uiFND_Data % 10];
 	while(1)
 	{
-		if(flag)
-		{
-			FND_sel++;
-			FND_sel %= 4;
-			flag = 0;
-		}
 	}
 }
 
