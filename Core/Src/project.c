f3ec7ae59f0706ac0b9c12733127e9c1cda7a129
SungYG0612 <songyoungwan1903@gmail.com>
2025-06-10 14:46:04 +0900

    4fnd_10Hz.2

--- Core/Src/project.c
+++ Core/Src/project.c
@@ -4,7 +4,7 @@
 //////////////////////////////////
 /*variable*/
 int flag = 0;
-const int32_t FND_Number[10] = {0x82007d00,0xdb002400,0x1600e900,0x1a00e500,
+const int32_t FND_Table[10] = {0x82007d00,0xdb002400,0x1600e900,0x1a00e500,
 		0x4b00b400,0x2a00d500,0x2200dd00,0x9b006400,0x0200fd00,0x0a00f500};
 unsigned int uiFND_Data = 0;
 unsigned int FND_Buf[4]={};
@@ -39,8 +39,8 @@ void Initialize(void)
 
 void Number_Transform(unsigned int input)
 {
-	FND_Buf[0] = FND_Number[input / 1000];
-	FND_Buf[1] = FND_Number[(input % 1000) / 100];
-	FND_Buf[2] = FND_Number[(input % 100) / 10];
-	FND_Buf[3] = FND_Number[input % 10];
+	FND_Buf[0] = FND_Table[input / 1000];
+	FND_Buf[1] = FND_Table[(input % 1000) / 100];
+	FND_Buf[2] = FND_Table[(input % 100) / 10];
+	FND_Buf[3] = FND_Table[input % 10];
 }
