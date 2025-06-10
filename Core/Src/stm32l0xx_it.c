4fb878f055dc954630cb2f5ae1c57006803106bf
SungYG0612 <songyoungwan1903@gmail.com>
2025-06-10 14:58:52 +0900

    empty

--- Core/Src/stm32l0xx_it.c
+++ Core/Src/stm32l0xx_it.c
@@ -20,7 +20,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32l0xx_it.h"
-#include "variable.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
@@ -58,7 +57,7 @@
 /* External variables --------------------------------------------------------*/
 
 /* USER CODE BEGIN EV */
-extern int time_value;
+
 /* USER CODE END EV */
 
 /******************************************************************************/
@@ -126,15 +125,11 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
-if(time_value >= 500)
-{
-	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-	time_value = 0;
-}
+
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
   /* USER CODE BEGIN SysTick_IRQn 1 */
-time_value += 1;
+
   /* USER CODE END SysTick_IRQn 1 */
 }
 
