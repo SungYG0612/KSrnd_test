c5751f310d97331f6833b5f64b101f7435ca0c94
SungYG0612 <songyoungwan1903@gmail.com>
2025-06-11 15:33:16 +0900

    button.4.6
    Leading Zero
    찐완

--- Core/Src/project.c
+++ Core/Src/project.c
@@ -31,9 +31,9 @@ void project_main(void)
 	{
 		if(Flag=='1')
 		{
-			if(uiPin_value & 0x0400) {iNumber_Buf--;}		//1번 버튼 = iNumber_Buf 증가
-			if(uiPin_value & 0x1000) {iNumber_Buf++;}		//3번 버튼 = iNumber_Buf 감소
-			if(uiPin_value & 0x4000) {iNumber_Buf=0;}		//5번 버튼 = iNumber_Buf 0 초기화
+			if(uiPin_value == 0x0400) {iNumber_Buf--;}		//1번 버튼 = iNumber_Buf 증가
+			if(uiPin_value == 0x1000) {iNumber_Buf++;}		//3번 버튼 = iNumber_Buf 감소
+			if(uiPin_value == 0x4000) {iNumber_Buf=0;}		//5번 버튼 = iNumber_Buf 0 초기화
 			if(iNumber_Buf>9999 || iNumber_Buf<-999) {iNumber_Buf = 0;}		//iNumber_Buf가 9999보다 크고 -999보다 작다면 0으로 초기화
 			Number_convert(iNumber_Buf);
 			Flag = '0';
@@ -44,39 +44,52 @@ void project_main(void)
 //////////////////////////////////
 void Initialize(void)
 {
-	GPIOB->BSRR = 0xe0001000;
-	GPIOE->BSRR = FND_Table[0];
 }
 
 void Number_convert(int input_number)		//숫자 변환 함수
 {
+	unsigned int uiNumber_state[4] = {};
+	//양수 음수 확인 Code
 	if(input_number<0)		//iNumber_Buf가 음수일 경우
 	{
-		cNumber_sign = '-';		//숫자 부호 저장 변수에 '-'저장
 		input_number *= -1;		//음수 X -1로 양수로 변환
+		cNumber_sign = '-';
+	}
+	else {cNumber_sign = '+';}
+
+	if(input_number >= 1000)
+	{
+		uiNumber_state[0] = FND_Table[input_number / 1000];
+		uiNumber_state[1] = FND_Table[(input_number % 1000) / 100];
+		uiNumber_state[2] = FND_Table[(input_number % 100) / 10];
+		uiNumber_state[3] = FND_Table[input_number % 10];
+	}
+	else if(input_number >= 100)
+	{
+		uiNumber_state[0] = 0xff000000;
+		uiNumber_state[1] = FND_Table[(input_number % 1000) / 100];
+		uiNumber_state[2] = FND_Table[(input_number % 100) / 10];
+		uiNumber_state[3] = FND_Table[input_number % 10];
+	}
+	else if(input_number >= 10)
+	{
+		uiNumber_state[0] = 0xff000000;
+		uiNumber_state[1] = 0xff000000;
+		uiNumber_state[2] = FND_Table[(input_number % 100) / 10];
+		uiNumber_state[3] = FND_Table[input_number % 10];
 	}
 	else
 	{
-		cNumber_sign = '+';
-		uiDisplay_Buf[0] = FND_Table[input_number / 1000];
+		uiNumber_state[0] = 0xff000000;
+		uiNumber_state[1] = 0xff000000;
+		uiNumber_state[2] = 0xff000000;
+		uiNumber_state[3] = FND_Table[input_number % 10];
 	}
 
-	uiDisplay_Buf[1] = FND_Table[(input_number % 1000) / 100];
-	uiDisplay_Buf[2] = FND_Table[(input_number % 100) / 10];
-	uiDisplay_Buf[3] = FND_Table[input_number % 10];
-
-	//Leading Zero Code
-	if(uiDisplay_Buf[0] == FND_Table[0]|| cNumber_sign == '-')	//0번 버퍼가 0이거나 부호가 '-'이면
-	{
-		if(uiDisplay_Buf[1] == FND_Table[0])						//0번 버퍼가 0이거나 부호가 '-'이고, 1번 버퍼가 0이면
-		{
-			if(uiDisplay_Buf[2] == FND_Table[0])					//0번 버퍼가 0이거나 부호가 '-'이고, 1번 버퍼가 0이고, 2번 버퍼가 0이면
-			{
-				uiDisplay_Buf[2] = (uiDisplay_Buf[2] & 0xffff00ff) | 0xff000000;		//2번 FND 미표시
-			}
-			uiDisplay_Buf[1] = (uiDisplay_Buf[1] & 0xffff00ff) | 0xff000000;			//1번 FND 미표시
-		}
-		if(cNumber_sign == '-') {uiDisplay_Buf[0] = 0x7f008000;}		//0번 FND '-' 표시
-		else {uiDisplay_Buf[0] = (uiDisplay_Buf[0] & 0xffff00ff) | 0xff000000;}							//0번 FND 미표시
-	}
+	if(cNumber_sign == '-') {uiDisplay_Buf[0] = 0x7f008000;}
+	else {uiDisplay_Buf[0] = uiNumber_state[0];}
+	uiDisplay_Buf[1] = uiNumber_state[1];
+	uiDisplay_Buf[2] = uiNumber_state[2];
+	uiDisplay_Buf[3] = uiNumber_state[3];
 }
+
